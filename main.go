a5c017c1594065f50288747eecb4698291d26fc2
hpazk <hpazk.zkh@gmail.com>
2021-02-04 13:44:38 +0700

    deploy branch

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+type M map[string]interface{}
 
 func main() {
 	fmt.Println("running...")
+
+	e := echo.New()
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
+
+	e.GET("/users", func(c echo.Context) error {
+		meta := M{"message": "success"}
+		return c.JSON(http.StatusOK, meta)
+	})
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
